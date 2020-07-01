fa4f929107e526af6e6a406874af7693bd990057
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-07-01 22:38:07 +0400

    mirror comit

--- ws/room.ts
+++ ws/room.ts
@@ -1,17 +1,30 @@
-import { WebSocket } from "https://deno.land/std/ws/mod.ts";
+import { WebSocket, isWebSocketCloseEvent } from "https://deno.land/std/ws/mod.ts";
 import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 const socket = new Map<string, WebSocket>()
 
 const chatConn = async (ws: WebSocket) => {
-    console.log('Socket COnnection');
+    console.log('Socket Connection');
 
     //new ws connection
-
     const uid = v4.generate();
     socket.set(uid, ws);
 
-    console.log(socket);
+    //
+    for await (const ev of ws) {
+        console.log('******', ev);
+        
+        //If conn closed, del socket
+        if ( isWebSocketCloseEvent(ev) ) {
+            socket.delete(uid);
+        }
+
+        //Create Ev object
+        if( typeof ev === 'string' ) {
+            let evObj = JSON.parse(ev);
+            console.log('EVOBJ',evObj)
+        }
+    }
 }
 
 export { chatConn };
